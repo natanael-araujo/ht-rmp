289dd4fdb09f279747d7047dc76c62fb20628868
Natanael de Araújo <internael@gmail.com>
2011-02-01 04:11:44 -0200

    day_2_spec.rb, contexts 1, 2, 3, 4 and 5 resolved

---
+++ files/day_2.rb
@@ -0,0 +1,30 @@
+require "spec_helper"
+
+#1
+def send_message
+  Message.new.send(:deliver)
+end
+
+#2
+def send_message_with_args
+  Message.__send__(:send, Message.new, 2)
+end
+
+#3
+def send_private_method
+  Message.new.send(:prepare)
+end
+
+#4
+def send_public_method
+  Message.new.prepare
+end
+
+#5 fatorar
+class Song
+
+  attr_accessor :title, :artist, :duration
+  def initialize(hash)
+    [:title, :artist, :duration].each {|attr| self.send(attr.to_s+"=", hash.fetch(attr))}
+  end
+end
